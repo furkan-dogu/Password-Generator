1d0e9f40cbd760a4ddf461356f5f31314b2f754d
Furkan Doğu <furkandogu2018@gmail.com>
2023-11-15 12:53:28 +0300

    bitti

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-//* 3 numara
-//* 2 sembol
-//* 1 kucuk harf
-//* 1 buyuk harf
-//* toplam 10 harf
+const p = document.querySelector("p")
+const reset = document.querySelector(".reset")
+const genPas = document.querySelector(".generate-password")
+const copy = document.querySelector(".copy")
+
+const createPass = () => {
+    let upper = ""
+        for(let i = 65; i <= 90; i++) {
+            upper += String.fromCharCode(i)
+        }
+        
+    const upperCase = upper;
+    const lowerCase = upper.toLowerCase();
+    const numbers = "1234567890"
+    const sembols = "!@#$%^&*()\\_+~|}{[]:;?><,./-=";
+    const allChar = upperCase + lowerCase + numbers + sembols
+
+    let pas = ""
+    let password = ""
+
+    pas += upperCase[Math.floor(Math.random() * upperCase.length)]
+    pas += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+
+    for(i = 0; i < 3; i++) {
+        pas += numbers[Math.floor(Math.random() * numbers.length)]
+    }
+
+    for(i = 0; i < 2; i++) {
+        pas += sembols[Math.floor(Math.random() * sembols.length)]
+    }
+
+    for(i = 0; i < 3; i++) {
+        pas += allChar[Math.floor(Math.random() * allChar.length)]
+    }
+
+    for(i = 0; i < 10; i++) {
+        let random = pas[Math.floor(Math.random() * pas.length)]
+        password += random
+        pas = pas.replace(random, "")
+    }
+    return password
+}
+
+genPas.addEventListener("click", () => {
+    p.textContent = createPass()
+})
+
+reset.addEventListener("click", () => {
+    p.textContent = "**********"
+})
+
+copy.addEventListener("click", () => {
+    navigator.clipboard.writeText(p.textContent)
+})
